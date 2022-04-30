a5d5b2728ea320800a1e4588116520793deef8d0
Intiser Rajoan Parash <irparash@gmail.com>
2022-04-30 18:18:48 -0500

    added sybbure, ethan and jon

--- syp_teensy/Syp_Teensy.ino
+++ syp_teensy/Syp_Teensy.ino
@@ -1,6 +1,6 @@
 /***************************************************
     Syp's main code (Teensy, SPI)
-    Last Updated: February 25, 2022
+    Last Updated: April 29, 2022
  ****************************************************/
 
 // Screen dimensions
@@ -8,34 +8,38 @@
 #define SCREEN_HEIGHT 128
 
 // Plug in the display to the Teensy's digital pins
-#define SCLK_PIN 13  // CLK = SPI Clock (SCK)
-#define MOSI_PIN 11  // DIN = Data Input (MOSI = SPI Master out Slave in)
-#define DC_PIN   9  // DC
-#define CS_PIN   10  // CS = Chip Select
-#define RST_PIN  15  // RST = Reset
+#define SCLK_PIN 13 // CLK = SPI Clock (SCK)
+#define MOSI_PIN 11 // DIN = Data Input (MOSI = SPI Master out Slave in)
+#define DC_PIN 9    // DC
+#define CS_PIN 10   // CS = Chip Select
+#define RST_PIN 15  // RST = Reset
+
+const int BUTTON = 2; // Pushbutton
 
 // Color definitions
-#define BLACK           0x0000
-#define BLUE            0x001F
-#define RED             0xF800
-#define GREEN           0x07E0
-#define CYAN            0x07FF
-#define MAGENTA         0xF81F
-#define YELLOW          0xFFE0
-#define WHITE           0xFFFF
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
 
 #include <Adafruit_GFX.h>
-
 #include <Adafruit_SSD1351.h>
 #include <SPI.h>
 
-// Option 1: use any pins but a little slower
-//Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
+// external animations
+extern uint8_t *nike[6];
+// extern uint8_t *wink[48];
+extern uint8_t *cake[5];
+extern uint8_t *smilee[10];
+extern uint8_t *face[1];
+extern uint8_t *ethan[1];
+extern uint8_t *jon[1];
+extern uint8_t *sybbure[1];
 
-// Option 2: must use the hardware SPI pins -- we'll come back to this later <<THIS IS WHAT WE HAVE RIGHT NOW>>
-// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
-// an output. This is much faster - also required if you want
-// to use the microSD card (see the image drawing example)
 Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
 GFXcanvas16 display(128, 128);
 
@@ -51,45 +55,67 @@ unsigned long blinkTime = 0;
 int blinkInterval = 5000;
 int sensorValue = 0;
 
-void setup(void) {
-  Serial.begin(9600);
-  Serial.println("starting setup!");
-  tft.begin();
-  delay(500);
-  tft.fillScreen(BLACK);
-  glitch();
-
-  Serial.println("setup finished!");
+void setup(void)
+{
+    Serial.begin(9600);
+    Serial.println("starting setup!");
+    tft.begin();
+    delay(500);
+    tft.fillScreen(BLACK);
+    sendText("Resetting...");
+    Serial.println("setup finished!");
 }
 
-void loop() {
-  //glitch();
-  //delay(1000);
+int buttonState = 0;
 
+void loop()
+{
+    // blink();
+    // wink();
 
-  //    if ( millis() - blinkTime > (blinkInterval + random(-1000, 50000))) {
-  //      if (random(0, 2) == 0) {
-  //
-  //        blink();
-  //        delay(20);
-  //        blink();
-  //
-  //      } else {
-  //        smile();
-  //      }
-  //      blinkTime = millis();
-  //    }
+    showThis(sybbure);
 
-  if (analogRead(A5) == 1023) {
-    pet();
-    blinkTime = millis();
-  } else if ( millis() - blinkTime > (blinkInterval + random(-1000, 50000))) {
-    blink();
-    delay(50);
-    //    blink();
-rotateRect();
-blinkTime = millis();
-  }
+    // tft.fillScreen(CYAN);
+}
+
+// Extra variables
+double iconDimension = 128;
+
+void showThis(uint8_t **animation)
+{
+    int animationSize = sizeof(animation) / sizeof(animation[0]);
+
+    // loop through the frames
+    for (unsigned int i = 0; i < animationSize; i++)
+    {
+        display.fillScreen(BLACK);
+        display.drawBitmap(0, 0, animation[i], iconDimension, iconDimension, CYAN);
+        tft.drawRGBBitmap(0, 0, display.getBuffer(), 128, 128);
+        delay(10);
+    }
+
+    // reverse loop
+    for (unsigned int i = animationSize; i > 0; i--)
+    {
+        display.fillScreen(BLACK);
+        display.drawBitmap(0, 0, animation[i - 1], iconDimension, iconDimension, CYAN);
+        tft.drawRGBBitmap(0, 0, display.getBuffer(), 128, 128);
+        delay(10);
+    }
+    delay(500);
+}
+
+// send text in the screen
+void sendText(String text)
+{
+    int xPos = SCREEN_WIDTH / 2 - 2 * text.length();
+
+    tft.fillScreen(BLACK);
+    tft.setCursor(xPos, SCREEN_HEIGHT / 2);
+    tft.setTextColor(CYAN);
+    tft.print(text);
+    delay(300);
+}
 
 
 }
@@ -114,7 +140,18 @@ void blink() {
   }
 }
 
-void smile() {
+// wink either left or right eye
+void wink()
+{
+
+    for (int i = 0; i < eyeHeight / 2; i += 5)
+    {
+
+        display.fillScreen(BLACK);
+        display.fillRoundRect(xPos, yPos + i, eyeWidth, eyeHeight - i * 2, eyeRound, CYAN);
+        display.fillRoundRect(SCREEN_WIDTH - (xPos + eyeWidth), yPos + i, eyeWidth, eyeHeight - i * 2, eyeRound, CYAN);
+        tft.drawRGBBitmap(0, 0, display.getBuffer(), 128, 128);
+    }
 
   display.fillScreen(BLACK);
   display.fillRoundRect(xPos, yPos, eyeWidth, eyeHeight, eyeRound, CYAN);
@@ -125,21 +162,12 @@ void smile() {
     display.fillRoundRect(SCREEN_WIDTH - (xPos + eyeWidth), yPos - i + eyeHeight, eyeWidth, eyeHeight, eyeRound * 2, BLACK);
     tft.drawRGBBitmap(0, 0, display.getBuffer(), 128, 128);
 
-  }
-
-  delay(1000);
-  glitch();
-  //drop down eyes after smile, replaced with glitch
-  //  for (int i = eyeHeight / 1.2; i >= 0; i -= 7) {
-  //    display.fillRoundRect(xPos, yPos, eyeWidth, eyeHeight, eyeRound, CYAN);
-  //    display.fillRoundRect(SCREEN_WIDTH - (xPos + eyeWidth), yPos, eyeWidth, eyeHeight, eyeRound, CYAN);
-  //
-  //    display.fillRoundRect(xPos, yPos - i + eyeHeight, eyeWidth, eyeHeight, eyeRound * 2, BLACK);
-  //    display.fillRoundRect(SCREEN_WIDTH - (xPos + eyeWidth), yPos - i + eyeHeight, eyeWidth, eyeHeight, eyeRound * 2, BLACK);
-  //    tft.drawRGBBitmap(0, 0, display.getBuffer(), 128, 128);
-  //
-  //  }
-
+void sleep()
+{
+    int sleepHeight = 6;
+    int sleepDepth = -2;
+    display.fillRoundRect(xPos, yPos, eyeWidth, eyeHeight, eyeRound, CYAN);
+    display.fillRoundRect(SCREEN_WIDTH - (xPos + eyeWidth), yPos, eyeWidth, eyeHeight, eyeRound, CYAN);
 
 }
 
@@ -276,22 +304,3 @@ void smallerEyes() {
 
   delay(2000);
 }
-
-// make the rectangle rotate
-void rotateRect() {
-  int rectY = 128;
-  int rectHeight = 128;
-
-  for (int i = 0; i < 128 / 2  ; i += 20) {
-    display.fillScreen(BLACK);
-    display.fillRect(0, rectY - rectHeight - i, 128, rectHeight, CYAN);
-    tft.drawRGBBitmap(0, 0, display.getBuffer(), 128, 128);
-  }
-
-  tft.fillScreen(BLACK);
-  tft.fillRoundRect(xPos, yPos, eyeWidth, eyeHeight, eyeRound, CYAN);
-  tft.fillRoundRect(SCREEN_WIDTH - (xPos + eyeWidth), yPos, eyeWidth, eyeHeight, eyeRound , CYAN);
-}
-
-
-
